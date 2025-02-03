c1cdffd14cd8fd1a8d751a7d95c7b605ceaf9670
Nishil Visawadia <nmvisawadia@gmail.com>
2025-02-03 11:11:44 +0530

    refactor: improve code formatting and readability in tam.js and dashboard.html

--- assets/js/tam.js
+++ assets/js/tam.js
@@ -1,6 +1,6 @@
 
 const img1 = document.getElementById("imgBack1")
-img1.addEventListener("click", function(){
+img1.addEventListener("click", function () {
     document.getElementById('totalAddresable').click();
 })
 
@@ -32,18 +32,18 @@ function removeRow_TAM() {
         table_TAM.deleteRow(rows.length - 1);
         populate_total_tam()
     }
-    
+
 }
 
 // Fetching data from table and inserting into localstorage
 function retrieveTableDataTAM() {
-    
+
     // Get the table element
     const table_TAM = document.getElementById("tableDiv_TAM").querySelector("table");
-    
+
     // Initialize an array to store the data
     const tableData = [];
-    
+
     // Iterate through each row of the table
     for (let i = 1; i < table_TAM.rows.length; i++) {
         const row = table_TAM.rows[i];
@@ -52,7 +52,7 @@ function retrieveTableDataTAM() {
         // Iterate through each cell of the row
         for (let j = 0; j < row.cells.length; j++) {
             const cell = row.cells[j];
-           
+
             // Retrieve the input value from the cell and store it in the rowData object
             rowData["column" + j] = cell.querySelector("input").value;
         }
@@ -60,133 +60,140 @@ function retrieveTableDataTAM() {
         tableData.push(rowData);
 
     }
-    
+
     const jsonString = JSON.stringify(tableData)
-    localStorage.setItem('data',jsonString);
-    
-    swal({text:"TAM data has been saved",showConfirmButton:false});
-    console.log(document.getElementById('total_tam').innerText+"total tam");
+    localStorage.setItem('data', jsonString);
+
+    swal({ text: "TAM data has been saved", showConfirmButton: false });
+    console.log(document.getElementById('total_tam').innerText + "total tam");
     // Return the tableData array containing all the table data
 
     localStorage.setItem('total_tam', document.getElementById('total_tam').innerText);
     return tableData;
-   
+
 
 }
 
 
 
 
-function populate_TAM(){
+function populate_TAM() {
     const table_TAM = document.getElementById("tableDiv_TAM").querySelector("table");
     //deleting all rows'
     var rows = table_TAM.getElementsByTagName("tr");
-    for (var i = rows.length-1; i > 0; i--) {
+    for (var i = rows.length - 1; i > 0; i--) {
         table_TAM.deleteRow(i);
     }
 
-    var data=JSON.parse(localStorage.getItem('data'));
+    var data = JSON.parse(localStorage.getItem('data'));
     // Iterate through each row of the table
-   
-    if(data==null){
-        addRow_TAM()
-        addRow_TAM()
-    }  else {
-  
-    
-    for (let i = 0; i < data.length; i++) {
 
-        const newRow = table_TAM.insertRow();
-        for (let key in data[i]) {
-            // Insert a new cell for each property
-            console.log(data[i],key)
-            const newCell = newRow.insertCell();
-            // Set the innerText of the cell to the value of the current property
-            
-            if(key=='column1'){
-                newCell.innerHTML = '<input class="inputs" onchange="populate_total_tam()" placeholder="Enter Name of Customer Segment" value='+ data[i][key]+'   >';
-            }else{
-                newCell.innerHTML = '<input class="inputs" placeholder="Enter Name of Customer Segment" value='+ data[i][key]+'  >';
+    if (data == null) {
+        addRow_TAM()
+        addRow_TAM()
+    } else {
+
+
+        for (let i = 0; i < data.length; i++) {
+
+            const newRow = table_TAM.insertRow();
+            for (let key in data[i]) {
+                // Insert a new cell for each property
+                console.log(data[i], key)
+                const newCell = newRow.insertCell();
+                // Set the innerText of the cell to the value of the current property
+
+                if (key == 'column1') {
+                    // newCell.innerHTML = '<input class="inputs" onchange="populate_total_tam()" placeholder="Enter Name of Customer Segment" value='+ data[i][key]+'   >';
+                    newCell.innerHTML = '<input class="inputs" type="text" placeholder="Enter Numerical Value" value="' + data[i][key] + '" >';
+                    newCell.querySelector("input").addEventListener("change", function () {
+                        this.value = addCommas(this.value);
+                        populate_total_tam();
+                    });
+                } else {
+                    newCell.innerHTML = '<input class="inputs" placeholder="Enter Name of Customer Segment" value=' + data[i][key] + '  >';
+                }
             }
         }
+        if (localStorage.getItem('total_tam') === "undefined") {
+            document.getElementById('total_tam').innerText = 0;
+        } else {
+            document.getElementById('total_tam').innerText = localStorage.getItem('total_tam');
+        }
     }
-    if(localStorage.getItem('total_tam')==="undefined"){
-        document.getElementById('total_tam').innerText=0;
-    }else{
-        document.getElementById('total_tam').innerText=localStorage.getItem('total_tam');    
-    }
-    }
 }
 
 
-function get_business_name(){
-    b_name=document.getElementById("BusinessName");
-    localStorage.setItem("b_name",b_name.value);
-    swal({text:"Business name is saved",showConfirmButton:false});
-    
+function get_business_name() {
+    b_name = document.getElementById("BusinessName");
+    localStorage.setItem("b_name", b_name.value);
+    swal({ text: "Business name is saved", showConfirmButton: false });
+
 }
 
-function populate_business_name(){
-    document.getElementById("BusinessName").value=localStorage.getItem("b_name");
-    if(localStorage.getItem("ebt")==null){
-        document.getElementById("ebt").value=0
-    }else{ 
-       
-    document.getElementById("ebt").innerText=localStorage.getItem("ebt");
-    
+function populate_business_name() {
+    document.getElementById("BusinessName").value = localStorage.getItem("b_name");
+    if (localStorage.getItem("ebt") == null) {
+        document.getElementById("ebt").value = 0
+    } else {
+
+        document.getElementById("ebt").innerText = localStorage.getItem("ebt");
+
     }
 
-if(localStorage.getItem("runway")==null){
-   
-    document.getElementById("runway").innerText=0
-}else{ 
-   
-document.getElementById("runway").innerText=localStorage.getItem("runway");
+    if (localStorage.getItem("runway") == null) {
+
+        document.getElementById("runway").innerText = 0
+    } else {
+
+        document.getElementById("runway").innerText = localStorage.getItem("runway");
+
+    }
+
+    if (localStorage.getItem("runway") == null) {
+
+        document.getElementById("runway").innerText = 0
+    } else {
+
+        document.getElementById("runway").innerText = localStorage.getItem("runway");
+
+    }
+
+    if (localStorage.getItem("emi") == null) {
+        document.getElementById("runway").innerText = 0
+    } else {
+        document.getElementById('emi_table').style.visibility = 'visible';
+        document.getElementById("runway").innerText = localStorage.getItem("runway");
+    }
+
+
+
 
 }
 
-if(localStorage.getItem("runway")==null){
-   
-    document.getElementById("runway").innerText=0
-}else{ 
-   
-document.getElementById("runway").innerText=localStorage.getItem("runway");
-
-}
-
-if(localStorage.getItem("emi")==null){
-    document.getElementById("runway").innerText=0
-}else{ 
-document.getElementById('emi_table').style.visibility='visible';
-document.getElementById("runway").innerText=localStorage.getItem("runway");
-}
 
 
 
 
-}
-
-
-
-
-
-function populate_total_tam(){
-    var total_tam=0
+function populate_total_tam() {
+    var total_tam = 0
     const table_TAM = document.getElementById("tableDiv_TAM").querySelector("table");
     // Iterate through each row of the table
     for (let i = 1; i < table_TAM.rows.length; i++) {
         const row = table_TAM.rows[i];
         console.log(row);
-        total_tam=total_tam+parseInt(row.cells[1].querySelector("input").value);
+        console.log(row.cells[1].querySelector("input").value.replace(",", ""));
+        total_tam = total_tam + parseInt(row.cells[1].querySelector("input").value.replace(",", ""));
+        console.log(total_tam);
 
-} 
-document.getElementById('total_tam').innerText=addCommas(total_tam);
+    }
+    document.getElementById('total_tam').innerText = addCommas(total_tam);
 
 }
 
 
 
-window.onload=populate_business_name();
+window.onload = populate_business_name();
 
 // Add event listeners
 addButton_TAM.addEventListener("click", addRow_TAM);
